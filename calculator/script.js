b73c440c24a039dff330c327d941c82cf99df0f5
Brea Mikel <59631565+breamikel@users.noreply.github.com>
2020-06-09 23:44:10 -0700

    Update script.js
    fix pow and sqrt button

--- calculator/script.js
+++ calculator/script.js
@@ -1,27 +1,23 @@
 var val1, val2;
-var vals = [];
 function loadDisplay(num) {
     document.getElementById('display2').value += num.value.toString();
 }
+
 function delFromRight() {
-    document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
+    document.getElementById('display2').value = document.getElementById('display2').value.slice(0, -1);
 }
 function clearDisplay() {
-    document.getElementById('display2').value = ""
-
+    document.getElementById('display2').value = "";
 }
-function setOperator(trg) {
-    val1 = document.getElementById('display2').value+ " " + trg.value;
+function setOperator(tag) {
+    val1 = document.getElementById('display2').value + " " + tag.value;
     document.getElementById('display1').innerHTML = val1;
     document.getElementById('display2').value = "";
-    vals.push(val1)
-
 }
-function doMath(){
-   
+function doMath() {
     val2 = document.getElementById('display2').value;
     var result;
-    switch(val1.slice(-1)) {
+    switch(val1.slice(-1)){
         case '+':
             result = parseInt(val1) + parseInt(val2);
             break;
@@ -34,11 +30,11 @@ function doMath(){
         case '/':
             result = parseInt(val1) / parseInt(val2);
             break;
-        case 'sqrt':
+        case 's':
             result = Math.sqrt(parseInt(val1));
             break;
-        case 'pow':
-            result = Math.pow(parseInt(val2), parseInt(val2));
+        case 'p':
+            result = Math.pow(parseInt(val1)), parseInt(val2);
             break;
     }
     document.getElementById('display1').innerHTML = "";
